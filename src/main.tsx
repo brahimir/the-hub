359604be94ebe9befd442839a0f53061f2f64be4
Nick Brahimir <nickb@webility.ca>
2023-07-13 09:04:53 -0400

    Adding color theming

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import theme from "./theme.ts";
 // Chakra
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
