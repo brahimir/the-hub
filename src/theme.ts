c009f67e65ef2195b89ff8d7dc5d36bf9868245e
Nick Brahimir <nickb@webility.ca>
2023-07-18 12:49:28 -0400

    Updating ChakraUI color themeing

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,26 @@ const config: ThemeConfig = {
   initialColorMode: "dark"
 };
 
-const theme = extendTheme({ config });
+// See https://chakra-ui.com/docs/styled-system/theme
+// See https://smart-swatch.netlify.app/ for color gradient generator
+const customColors = {
+  gray: {
+    50: "#f9f9f9",
+    100: "#ededed",
+    200: "#d3d3d3",
+    300: "#b3b3b3",
+    400: "#a0a0a0",
+    500: "#898989",
+    600: "#6c6c6c",
+    700: "#202020",
+    800: "#121212",
+    900: "#111"
+  }
+};
+
+const theme = extendTheme({
+  config,
+  colors: customColors
+});
 
 export default theme;
