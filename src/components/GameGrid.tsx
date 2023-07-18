7c4c2a8ef1b26b0e27208ffd6df4b36680acf7bb
Nick Brahimir <nickb@webility.ca>
2023-07-18 12:51:03 -0400

    cleanup

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,20 +14,17 @@ interface GameGridProps {
 const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { games, error, isLoading } = useGames(gameQuery);
 
+  if (error)
+    return (
+      <Text color={"red"}>There was an error loading games. Please refresh the page and try again.</Text>
+    );
+
   return (
-    <>
-      {error && (
-        <Text color={"red"}>
-          There was an error loading games. Please refresh the page and try again.
-        </Text>
-      )}
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={"0.5rem"} spacing={5}>
+      {isLoading && <GameCardSkeleton />}
 
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={"0.5rem"} spacing={5}>
-        {isLoading && <GameCardSkeleton />}
-
-        {games && games.map((game) => <GameCard key={game.id} game={game} />)}
-      </SimpleGrid>
-    </>
+      {games && games.map((game) => <GameCard key={game.id} game={game} />)}
+    </SimpleGrid>
   );
 };
 
